c6b63ea1e101b0688aa033241a3df2a284735ea2
Ananta <codeantu@gmail.com>
2024-11-30 21:45:12 +0530

    Remove Test.java and add Prim.java and One.java with basic examples of primitive data types and user input handling

---
+++ One.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class One {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    // int a, b;
+
+    // System.out.println("Enter 2 numbers : ");
+
+    // a = sc.nextInt();
+    // b = sc.nextInt();
+
+    // System.out.println("Sum of " + a + " and " + b + " is " + (a + b));
+
+    String name;
+
+    name = sc.next();
+
+    System.out.println("Hello " + name);
+
+  }
+
+}
